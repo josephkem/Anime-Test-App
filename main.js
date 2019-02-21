9f8faddc4fdb337931f59646436ca11b2e63e545
josephkem <josephkemoy@gmail.com>
2019-02-21 18:39:22 -0500

    Added 3 more animations to drop all 4 shapes and added a delay to synchronize them dropping and returning one at a time

--- main.js
+++ main.js
@@ -8,4 +8,43 @@ anime ({
         value: '1turn',
         easing: 'easeInOutSine'
     }
-})
+});
+
+anime ({
+    targets: 'div.box.blue',
+    translateY: [
+        {value: 200, duration: 500, delay: 1100},
+        {value: 0, duration: 500}
+    ],
+    rotate:{
+        value: '1turn',
+        easing: 'easeInOutSine',
+        delay: 1000
+    }
+});
+
+anime ({
+    targets: 'div.box.green',
+    translateY: [
+        {value: 200, duration: 500, delay: 2100},
+        {value: 0, duration: 500}
+    ],
+    rotate:{
+        value: '1turn',
+        easing: 'easeInOutSine',
+        delay: 2000
+    }
+});
+
+anime ({
+    targets: 'div.box.yellow',
+    translateY: [
+        {value: 200, duration: 500, delay: 3100},
+        {value: 0, duration: 500}
+    ],
+    rotate:{
+        value: '1turn',
+        easing: 'easeInOutSine',
+        delay: 3000
+    }
+});
