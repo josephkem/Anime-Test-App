39ee7868748e968e5124ff258902d41433e6c319
josephkem <josephkemoy@gmail.com>
2019-02-21 18:23:20 -0500

    Added an animation to the first box to drop and spin simultaneously

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+anime ({
+    targets: 'div.box.red',
+    translateY: [
+        {value: 200, duration: 500},
+        {value: 0, duration: 500}
+    ],
+    rotate:{
+        value: '1turn',
+        easing: 'easeInOutSine'
+    }
+})
