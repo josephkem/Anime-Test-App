cf3edba1f3bdb9552d78c9a9d72b450364651b23
josephkem <josephkemoy@gmail.com>
2019-02-21 20:57:52 -0500

    Reduced anime statements to one and added a function to run through the div objects

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-anime ({
-    targets: 'div.box.red',
+const playpause = anime ({
+    targets: 'div.box',
     translateY: [
         {value: 200, duration: 500},
         {value: 0, duration: 500}
@@ -7,44 +7,8 @@ anime ({
     rotate:{
         value: '1turn',
         easing: 'easeInOutSine'
-    }
+    },
+    delay: (el,i,l) => {return i * 1000},
+    autoplay: false
 });
 
-anime ({
-    targets: 'div.box.blue',
-    translateY: [
-        {value: 200, duration: 500, delay: 1100},
-        {value: 0, duration: 500}
-    ],
-    rotate:{
-        value: '1turn',
-        easing: 'easeInOutSine',
-        delay: 1000
-    }
-});
-
-anime ({
-    targets: 'div.box.green',
-    translateY: [
-        {value: 200, duration: 500, delay: 2100},
-        {value: 0, duration: 500}
-    ],
-    rotate:{
-        value: '1turn',
-        easing: 'easeInOutSine',
-        delay: 2000
-    }
-});
-
-anime ({
-    targets: 'div.box.yellow',
-    translateY: [
-        {value: 200, duration: 500, delay: 3100},
-        {value: 0, duration: 500}
-    ],
-    rotate:{
-        value: '1turn',
-        easing: 'easeInOutSine',
-        delay: 3000
-    }
-});
